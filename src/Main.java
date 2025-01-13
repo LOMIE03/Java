9b14dd9906e9b3654db3853d559a53842321c225
LOMIE03 <lommie@naver.com>
2025-01-13 17:31:02 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int i = 10;
+        int j = 10;
+
+
+        System.out.println(i == j);
+        System.out.println(i != j);
+        System.out.println(i < j);
+        System.out.println(i <= j);
+        System.out.println(i > j);
+        System.out.println(i >= j);
+
+        i += 5; // i = i + 10;
+
+        System.out.println(i);
+        System.out.println(i -=5);
+    }
+}
