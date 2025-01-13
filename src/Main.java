68db2f5db945492a09fc7fe543c5ae98029a3b2b
LOMIE03 <lommie@naver.com>
2025-01-13 16:35:11 +0900

    type

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+            boolean isFun = true;
+
+            char c = 'f';
+
+            int x = 59;
+
+            long bing = 34141244124L;
+
+            float f = 32.4f;
+
+            double d = 3123231.1415992;
+
+
+        System.out.println(d);
+    }
+}
