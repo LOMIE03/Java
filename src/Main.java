7590495a17af18f06338bd2c14002d38601e9c4b
LOMIE03 <lommie@naver.com>
2025-01-17 17:58:33 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int score = 85;
+        String result = (!(score>90)) ? "가" : "나";
+        System.out.println(result);
+    }
+}
