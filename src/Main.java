4b65d98f47a3090688d2e62f1c33485e90cbe4b8
LOMIE03 <lommie@naver.com>
2025-01-17 16:12:05 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int legthTop = 5;
+        int lengthBottom = 10;
+        int height = 7;
+        double area = (double)( legthTop + lengthBottom) * height / 2;
+        System.out.println(area);
+    }
+}
