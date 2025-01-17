da339f55a2ddb7cb1ad2a52607621cee69cf18e2
LOMIE03 <lommie@naver.com>
2025-01-17 17:28:44 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+       int var1 = 10;
+       int var2 = 3;
+       int var3 = 14;
+       double var4 = var1 * var1 * Double.parseDouble(var2 + "." + var3);
+        System.out.println("원의 넓이:" + var4);
+
+    }
+}
