8427c91b7e450f262ead488a276ac1154d6c85f7
LOMIE03 <lommie@naver.com>
2025-01-17 15:50:01 +0900

    first

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+       int value = 356;
+       System.out.println((int)((double)value * 0.01) * 100);
+       System.out.println(value * 0.01 * 100);
+       System.out.println(value / 100 * 100);
+    }
+}
