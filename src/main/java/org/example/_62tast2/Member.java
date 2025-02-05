84be3560b48e24b7fa945b618a5a299dcce1d8e2
LOMIE03 <lommie@naver.com>
2025-02-05 16:43:26 +0900

    first

---
+++ src/main/java/org/example/_62tast2/Member.java
@@ -0,0 +1,13 @@
+package org.example._62tast2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Member {
+    String name;
+    String id;
+    String password;
+    int age;
+
+}
---
+++ src/main/java/org/example/_62tast2/MemberExample.java
@@ -0,0 +1,17 @@
+package org.example._62tast2;
+
+public class MemberExample {
+    public static void main(String[] args) {
+        Member member = new Member();
+
+        member.name = "최하얀";
+        member.age = 23;
+
+        System.out.println(member.name);
+        System.out.println(member.age);
+    }
+
+
+
+
+}
