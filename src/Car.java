cb4f5f3e28ddcba6949c28508c17372e49dc119d
LOMIE03 <lommie@naver.com>
2025-02-06 15:40:16 +0900

    first

---
+++ src/Car.java
@@ -0,0 +1,27 @@
+public class Car {
+
+    String company = "현대자동차";
+    String model;
+    String color;
+    int maxSpeed;
+
+    Car(){
+
+    }
+
+    Car(String model) {
+        this.model = model;
+        this.color = "은색";
+    }
+
+    Car(String model, String color) {
+        this.model = model;
+        this.color = color;
+    }
+
+    Car(String model, String color, int maxSpeed) {
+        this.model = model;
+        this.color = color;
+        this.maxSpeed = maxSpeed;
+    }
+}
