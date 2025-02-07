ab653d5e491dba483b866861f203613b4ca59950
LOMIE03 <lommie@naver.com>
2025-02-07 16:26:30 +0900

    first

---
+++ src/MemberExample.java
@@ -0,0 +1,9 @@
+public class MemberExample {
+    public static void main(String[] args) {
+        Member user1 = new Member("홍길동", "hong");
+        System.out.print(user1.name + "\s");
+        System.out.print(user1.id);
+
+
+    }
+}
